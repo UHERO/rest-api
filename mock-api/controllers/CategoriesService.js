5732251d3ca16bb11d080af084926cec5eca29ce
Victoria Ward <vcward@hawaii.rr.com>
2016-08-23 16:17:37 -1000

    add additional series and observations

--- mock-api/controllers/CategoriesService.js
+++ mock-api/controllers/CategoriesService.js
@@ -101,6 +101,32 @@ exports.categorySeriesGET = function(args, res, next) {
     "seasonalAdjustmentShort" : "SA",
     "source" : "U.S. Dept. of Labor, Bureau of Labor Statistics",
     "sourceLink" : "http://data.bls.gov/cgi-bin/srgate"
+  }, {
+    "id" : "VIS@HON.A",
+    "title" : "Total Visitor Arrivals",
+    "observationStart" : "1990-01-01",
+    "observationEnd" : "2015-01-01",
+    "frequency" : "Annual",
+    "frequencyShort" : "A",
+    "units" : "Thousands",
+    "unitsShort" : "Thou.",
+    "SeasonalAdjustment" : "Not Seasonally Adjusted",
+    "seasonalAdjustmentShort" : "NSA",
+    "source" : "Hawaii Dept. of Business, Economic Development and Tourism",
+    "sourceLink" : "http://www.hawaii.gov/dbedt/info/visitor-stats/"
+  }, {
+    "id" : "VIS@HON.M",
+    "title" : "Total Visitor Arrivals",
+    "observationStart" : "1990-01-01",
+    "observationEnd" : "2015-01-01",
+    "frequency" : "Monthly",
+    "frequencyShort" : "M",
+    "units" : "Thousands",
+    "unitsShort" : "Thou.",
+    "SeasonalAdjustment" : "Not Seasonally Adjusted",
+    "seasonalAdjustmentShort" : "NSA",
+    "source" : "Hawaii Dept. of Business, Economic Development and Tourism",
+    "sourceLink" : "http://www.hawaii.gov/dbedt/info/visitor-stats/"
   } ]
 };
   if(Object.keys(examples).length > 0) {
--- mock-api/controllers/SeriesService.js
+++ mock-api/controllers/SeriesService.js
@@ -62,6 +62,32 @@ exports.seriesGET = function(args, res, next) {
     "seasonalAdjustmentShort" : "SA",
     "source" : "U.S. Dept. of Labor, Bureau of Labor Statistics",
     "sourceLink" : "http://data.bls.gov/cgi-bin/srgate"
+  }, {
+    "id" : "VIS@HON.A",
+    "title" : "Total Visitor Arrivals",
+    "observationStart" : "1990-01-01",
+    "observationEnd" : "2015-01-01",
+    "frequency" : "Annual",
+    "frequencyShort" : "A",
+    "units" : "Thousands",
+    "unitsShort" : "Thou.",
+    "SeasonalAdjustment" : "Not Seasonally Adjusted",
+    "seasonalAdjustmentShort" : "NSA",
+    "source" : "Hawaii Dept. of Business, Economic Development and Tourism",
+    "sourceLink" : "http://www.hawaii.gov/dbedt/info/visitor-stats/"
+  }, {
+    "id" : "VIS@HON.M",
+    "title" : "Total Visitor Arrivals",
+    "observationStart" : "1990-01-01",
+    "observationEnd" : "2015-01-01",
+    "frequency" : "Monthly",
+    "frequencyShort" : "M",
+    "units" : "Thousands",
+    "unitsShort" : "Thou.",
+    "SeasonalAdjustment" : "Not Seasonally Adjusted",
+    "seasonalAdjustmentShort" : "NSA",
+    "source" : "Hawaii Dept. of Business, Economic Development and Tourism",
+    "sourceLink" : "http://www.hawaii.gov/dbedt/info/visitor-stats/"
   } ]
 };
   if(Object.keys(examples).length > 0) {
@@ -82,7 +108,7 @@ exports.seriesObservationsGET = function(args, res, next) {
   **/
     var examples = {};
   examples['application/json'] = {
-  "observationStart" : "2014-01-01",
+  "observationStart" : "1995-01-01",
   "observationEnd" : "2015-01-01",
   "orderBy" : "observationDate",
   "sortOrder" : "desc",
@@ -94,6 +120,63 @@ exports.seriesObservationsGET = function(args, res, next) {
     }, {
       "date" : "2014-01-01",
       "value" : "627.225"
+    }, {
+      "date" : "2013-01-01",
+      "value" : "618.575"
+    }, {
+      "date" : "2012-01-01",
+      "value" : "606.283"
+    }, {
+      "date" : "2011-01-01",
+      "value" : "593.392"
+    }, {
+      "date" : "2010-01-01",
+      "value" : "586.908"
+    }, {
+      "date" : "2009-01-01",
+      "value" : "591.492"
+    }, {
+      "date" : "2008-01-01",
+      "value" : "619.233"
+    }, {
+      "date" : "2007-01-01",
+      "value" : "624.875"
+    }, {
+      "date" : "2006-01-01",
+      "value" : "617.108"
+    }, {
+      "date" : "2005-01-01",
+      "value" : "601.642"
+    }, {
+      "date" : "2004-01-01",
+      "value" : "583.408"
+    }, {
+      "date" : "2003-01-01",
+      "value" : "567.633"
+    }, {
+      "date" : "2002-01-01",
+      "value" : "556.792"
+    }, {
+      "date" : "2001-01-01",
+      "value" : "555.000"
+    }, {
+      "date" : "2000-01-01",
+      "value" : "551.358"
+    }, {
+      "date" : "1999-01-01",
+      "value" : "535.025"
+    }, {
+      "date" : "1998-01-01",
+      "value" : "531.283"
+    }, {
+      "date" : "1997-01-01",
+      "value" : "531.617"
+    }, {
+      "date" : "1996-01-01",
+      "value" : "530.733"
+    }, {
+      "date" : "1995-01-01",
+      "value" : "532.883"
     } ]
   }, {
     "transformation" : "pc1",
@@ -103,6 +186,63 @@ exports.seriesObservationsGET = function(args, res, next) {
     }, {
       "date" : "2014-01-01",
       "value" : "1.4"
+    }, {
+      "date" : "2013-01-01",
+      "value" : "2.0"
+    }, {
+      "date" : "2012-01-01",
+      "value" : "2.2"
+    }, {
+      "date" : "2011-01-01",
+      "value" : "1.1"
+    }, {
+      "date" : "2010-01-01",
+      "value" : "-0.8"
+    }, {
+      "date" : "2009-01-01",
+      "value" : "-4.5"
+    }, {
+      "date" : "2008-01-01",
+      "value" : "-0.9"
+    }, {
+      "date" : "2007-01-01",
+      "value" : "1.3"
+    }, {
+      "date" : "2006-01-01",
+      "value" : "2.6"
+    }, {
+      "date" : "2005-01-01",
+      "value" : "3.1"
+    }, {
+      "date" : "2004-01-01",
+      "value" : "2.8"
+    }, {
+      "date" : "2003-01-01",
+      "value" : "1.9"
+    }, {
+      "date" : "2002-01-01",
+      "value" : "0.3"
+    }, {
+      "date" : "2001-01-01",
+      "value" : "0.7"
+    }, {
+      "date" : "2000-01-01",
+      "value" : "3.1"
+    }, {
+      "date" : "1999-01-01",
+      "value" : "0.7"
+    }, {
+      "date" : "1998-01-01",
+      "value" : "-0.1"
+    }, {
+      "date" : "1997-01-01",
+      "value" : "0.2"
+    }, {
+      "date" : "1996-01-01",
+      "value" : "-0.4"
+    }, {
+      "date" : "1995-01-01",
+      "value" : "-0.6"
     } ]
   } ]
 };
