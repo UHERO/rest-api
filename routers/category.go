fd2c72a37bee0ebfa884b21061d86fe9a646510e
Jonathan Page <jonpage.econ@gmail.com>
2016-09-28 09:22:36 -1000

    category search by name

--- routers/category.go
+++ routers/category.go
@@ -7,9 +7,10 @@ import (
 )
 
 func SetCategoryRoutes(categoryRepository *data.CategoryRepository) *mux.Router {
-	categoryRouter := mux.NewRouter()
-	categoryRouter.HandleFunc("/v1/category", controllers.GetCategory(categoryRepository)).Methods("GET").Queries("id", "{id:[0-9]+}")
-	categoryRouter.HandleFunc("/v1/category", controllers.GetCategoriesByName(categoryRepository)).Methods("GET").Queries("search_text", "{searchText:.+}")
-	categoryRouter.HandleFunc("/v1/category", controllers.GetCategories(categoryRepository)).Methods("GET")
+	router := mux.NewRouter()
+	categoryRouter := router.Path("/v1/category").Methods("GET").Subrouter()
+	categoryRouter.Queries("id", "{id:[0-9]+").HandlerFunc(controllers.GetCategory(categoryRepository))
+	categoryRouter.Queries("search_text", "{searchText:.+}").HandlerFunc(controllers.GetCategoriesByName(categoryRepository))
+	categoryRouter.HandleFunc("", controllers.GetCategories(categoryRepository))
 	return categoryRouter
 }
