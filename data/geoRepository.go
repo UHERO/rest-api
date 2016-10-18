7172a17ad2c3e4e31defbc7a693e74e5db5ef17e
Jonathan Page <jonpage.econ@gmail.com>
2016-10-18 11:01:52 -1000

    updated geography repository and docs to support category geo endpoint

--- data/geoRepository.go
+++ data/geoRepository.go
@@ -10,7 +10,7 @@ type GeographyRepository struct {
 	DB *sql.DB
 }
 
-func (r *GeographyRepository) GetAllGeographies() (geographies []models.Geography, err error) {
+func (r *GeographyRepository) GetAllGeographies() (geographies []models.DataPortalGeography, err error) {
 	rows, err := r.DB.Query(`SELECT
 	fips, display_name, handle FROM geographies;`)
 	if err != nil {
@@ -26,7 +26,54 @@ func (r *GeographyRepository) GetAllGeographies() (geographies []models.Geograph
 		if err != nil {
 			continue
 		}
-		geographies = append(geographies, geography)
+		dataPortalGeography := models.DataPortalGeography{Handle: geography.Handle}
+		if geography.FIPS.Valid {
+			dataPortalGeography.FIPS = geography.FIPS.String
+		}
+		if geography.Name.Valid {
+			dataPortalGeography.Name = geography.Name.String
+		}
+		geographies = append(geographies, dataPortalGeography)
+	}
+	return
+}
+
+func (r *GeographyRepository) GetGeographiesByCategory(categoryId int64) (geographies []models.DataPortalGeography, err error) {
+	rows, err := r.DB.Query(
+		`SELECT
+		  geographies.fips, geographies.display_name_short,
+		  catgeo.chandle AS handle
+		FROM
+		  (SELECT DISTINCT(SUBSTRING_INDEX(SUBSTR(catnames.name, LOCATE('@', catnames.name) + 1), '.', 1)) AS chandle
+		    FROM
+		      (SELECT name
+		        FROM series
+		        WHERE (SELECT list FROM data_lists JOIN categories WHERE categories.data_list_id = data_lists.id AND categories.id = ?)
+		        LIKE CONCAT('%', LEFT(name, LOCATE("@", name)), '%')) AS catnames) AS catgeo
+		        LEFT JOIN geographies ON catgeo.chandle LIKE geographies.handle;`,
+		categoryId,
+	)
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		geography := models.Geography{}
+		err = rows.Scan(
+			&geography.FIPS,
+			&geography.Name,
+			&geography.Handle,
+		)
+		if err != nil {
+			continue
+		}
+		dataPortalGeography := models.DataPortalGeography{Handle: geography.Handle}
+		if geography.FIPS.Valid {
+			dataPortalGeography.FIPS = geography.FIPS.String
+		}
+		if geography.Name.Valid {
+			dataPortalGeography.Name = geography.Name.String
+		}
+		geographies = append(geographies, dataPortalGeography)
 	}
 	return
 }
--- routers/geo.go
+++ routers/geo.go
@@ -11,5 +11,6 @@ func SetGeographyRoutes(
 	geoRepository *data.GeographyRepository,
 ) *mux.Router {
 	router.HandleFunc("/v1/geo", controllers.GetGeographies(geoRepository)).Methods("GET")
+	router.HandleFunc("/v1/category/geo", controllers.GetGeographiesByCategory(geoRepository)).Methods("GET").Queries("id", "{id:[0-9]+}")
 	return router
 }
