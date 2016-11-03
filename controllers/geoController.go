649aea5e9bc72301e79dff2fdfed9a9140b8b943
Jonathan Page <jonpage.econ@gmail.com>
2016-11-02 19:41:31 -1000

    implemented series/siblings/geo

--- controllers/geoController.go
+++ controllers/geoController.go
@@ -86,3 +86,51 @@ func GetGeographiesByCategory(geographyRepository *data.GeographyRepository) fun
 		w.Write(j)
 	}
 }
+
+func GetSibllingGeographiesBySeriesId(geographyRepository *data.GeographyRepository) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		idParam, ok := mux.Vars(r)["id"]
+		if !ok {
+			common.DisplayAppError(
+				w,
+				errors.New("Couldn't get series id from request"),
+				"Bad request.",
+				400,
+			)
+			return
+		}
+		id, err := strconv.ParseInt(idParam, 10, 64)
+		if err != nil {
+			common.DisplayAppError(
+				w,
+				err,
+				"An unexpected error has occurred",
+				500,
+			)
+			return
+		}
+		geographies, err := geographyRepository.GetSeriesSiblingsGeoById(id)
+		if err != nil {
+			common.DisplayAppError(
+				w,
+				err,
+				"An unexpected error has occurred",
+				500,
+			)
+			return
+		}
+		j, err := json.Marshal(GeographiesResource{Data: geographies})
+		if err != nil {
+			common.DisplayAppError(
+				w,
+				err,
+				"An unexpected error processing JSON has occurred",
+				500,
+			)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(j)
+	}
+}
--- routers/geo.go
+++ routers/geo.go
@@ -11,6 +11,11 @@ func SetGeographyRoutes(
 	geoRepository *data.GeographyRepository,
 ) *mux.Router {
 	router.HandleFunc("/v1/geo", controllers.GetGeographies(geoRepository)).Methods("GET")
-	router.HandleFunc("/v1/category/geo", controllers.GetGeographiesByCategory(geoRepository)).Methods("GET").Queries("id", "{id:[0-9]+}")
+	router.HandleFunc("/v1/category/geo", controllers.GetGeographiesByCategory(geoRepository)).Methods("GET").Queries(
+		"id", "{id:[0-9]+}",
+	)
+	router.HandleFunc("/v1/series/siblings/geo", controllers.GetSibllingGeographiesBySeriesId(geoRepository)).Methods("GET").Queries(
+		"id", "{id:[0-9]+}",
+	)
 	return router
 }
