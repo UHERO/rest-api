aa7185e49b92de037b710931c83f568d1a8d57c8
Jonathan Page <jonpage.econ@gmail.com>
2016-11-14 17:43:08 -1000

    updated test

--- data/categoryRepository_test.go
+++ data/categoryRepository_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/UHERO/rest-api/models"
 	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 	"testing"
+	"fmt"
 )
 
 func TestGetAllCategories(t *testing.T) {
@@ -24,10 +25,10 @@ func TestGetAllCategories(t *testing.T) {
 		Name:     "Income",
 		Ancestry: sql.NullString{Valid: true, String: "1"},
 	}
-	categoryResult := sqlmock.NewRows([]string{"id", "name", "ancestry"}).
-		AddRow(category1.Id, category1.Name, nil).
-		AddRow(category2.Id, category2.Name, category2.Ancestry.String)
-	mock.ExpectQuery("SELECT id, name, ancestry FROM categories").
+	categoryResult := sqlmock.NewRows([]string{"id", "name", "ancestry", "geo", "freq"}).
+		AddRow(category1.Id, category1.Name, nil, "HI", "A").
+		AddRow(category2.Id, category2.Name, category2.Ancestry.String, nil, nil)
+	mock.ExpectQuery("SELECT (.+)").
 		WillReturnRows(categoryResult)
 
 	categoryRepository := CategoryRepository{DB: db}
