78611e1fa562257507969186c403036cb02b20fe
dji <David.Iannucci@hawaii.edu>
2017-03-14 18:53:10 -1000

    wip

--- data/cacheRepository.go
+++ data/cacheRepository.go
@@ -20,29 +20,25 @@ func (r *CacheRepository) ConnectCache() bool {
 	} else if r.authpw != "" {
 		if _, err = redis_conn.Do("AUTH", r.authpw); err != nil {
 			redis_conn.Close()
-//			redis_conn = nil
+			redis_conn = nil
 			log.Print("*** Redis authentication failure. No caching!")
 		}
 	}
-	if redis_conn != nil {
+	r.DB = redis_conn
+	if r.DB != nil {
 		log.Printf("Redis connection to %s established", r.server)
-		r.DB = redis_conn
 		return true
 	}
 	return false
-}
-
-func (r *CacheRepository) DisconnectCache() {
-	if r.DB != nil {
-		r.DB.Close()
-		r.DB = nil
-	}
 }
 
 func (r *CacheRepository) GetCache(key string) ([]byte, error) {
 	cval, err := r.DB.Do("GET", key)
 	if err != nil {
 		log.Printf("Redis error on GET: %v", err)
+		if r.ConnectCache() {
+			return r.GetCache(key)
+		}
 		return nil, err
 	}
 	if cval == nil {
@@ -55,6 +51,9 @@ func (r *CacheRepository) SetCache(key string, value []byte) (err error) {
 	resp, err := r.DB.Do("SET", key, value)
 	if err != nil {
 		log.Printf("Redis error on SET: %v", err)
+		if r.ConnectCache() {
+			return r.SetCache(key, value)
+		}
 		return
 	}
 	if resp != "OK" {
@@ -63,4 +62,11 @@ func (r *CacheRepository) SetCache(key string, value []byte) (err error) {
 		err = other_err
 	}
 	return
+}
+
+func (r *CacheRepository) DisconnectCache() {
+	if r.DB != nil {
+		r.DB.Close()
+		r.DB = nil
+	}
 }
