fb6eae6780f91aa29b62de82efcb5e667a84f02f
dji <David.Iannucci@hawaii.edu>
2017-10-02 14:25:02 -1000

    this seems preferred

--- models/models.go
+++ models/models.go
@@ -191,7 +191,7 @@ type Observation struct {
 	Date          time.Time
 	Value         sql.NullFloat64
 	PseudoHistory sql.NullBool
-	Decimals      int64
+	Decimals      int
 }
 
 type DataPortalObservation struct {
