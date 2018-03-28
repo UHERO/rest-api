1058781818bb1fac38b3ea2d2e4f8fd55b9823ce
dji <David.Iannucci@hawaii.edu>
2018-03-28 13:33:17 -1000

    wip

--- data/seriesRepository.go
+++ data/seriesRepository.go
@@ -159,9 +159,8 @@ var seriesPrefix = `SELECT
 	LEFT JOIN category_frequencies cf ON cf.category_id = categories.id
 	LEFT JOIN geographies ON
 	    (CASE WHEN EXISTS(SELECT * FROM category_geographies WHERE category_id = categories.id)
-	          THEN geographies.id = cg.geography_id
-	          ELSE true
-	    END)
+	          THEN geographies.id = cg.geography_id ELSE true
+	     END)
 	LEFT JOIN units ON units.id = series.unit_id
 	LEFT JOIN units AS measurement_units ON measurement_units.id = measurements.unit_id
 	LEFT JOIN sources ON sources.id = series.source_id
@@ -174,8 +173,7 @@ var seriesPrefix = `SELECT
 	AND NOT series.restricted
 	AND series.geography_id = geographies.id
 	AND (CASE WHEN EXISTS(SELECT * FROM category_frequencies WHERE category_id = categories.id)
-	          THEN series.frequency = cf.frequency
-	          ELSE true
+	          THEN series.frequency = cf.frequency ELSE true
 	     END)
 	AND (feature_toggles.status IS NULL OR NOT feature_toggles.status OR NOT series.quarantined)`
 //language=MySQL
@@ -466,10 +464,14 @@ func (r *SeriesRepository) GetFreqByCategory(categoryId int64) (frequencies []mo
 	LEFT JOIN data_list_measurements ON data_list_measurements.data_list_id = categories.data_list_id
 	LEFT JOIN measurement_series ON measurement_series.measurement_id = data_list_measurements.measurement_id
 	LEFT JOIN series ON series.id = measurement_series.series_id
+	LEFT JOIN category_frequencies cf ON cf.category_id = categories.id
 	LEFT JOIN feature_toggles ON feature_toggles.universe = series.universe AND feature_toggles.name = 'filter_by_quarantine'
 	WHERE categories.id = ?
 	AND NOT (categories.hidden OR categories.masked)
 	AND NOT series.restricted
+	AND (CASE WHEN EXISTS(SELECT * FROM category_frequencies WHERE category_id = categories.id)
+	          THEN series.frequency = cf.frequency ELSE true
+	     END)
 	AND (feature_toggles.status IS NULL OR NOT feature_toggles.status OR NOT series.quarantined)
 	ORDER BY FIELD(freq, "A", "S", "Q", "M", "W", "D");`, categoryId)
 	if err != nil {
