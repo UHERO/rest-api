3ae42587090d5aaa363220cf14c180f274823dac
dji <David.Iannucci@hawaii.edu>
2020-09-19 15:31:16 -1000

    wip

--- data/seriesRepository.go
+++ data/seriesRepository.go
@@ -732,7 +732,7 @@ func (r *FooRepository) GetSeriesByName(name string, universe string, expand boo
 	row, err := r.RunQuery(`
 		SELECT
 		   s.id, s.name, s.universe, s.description, frequency, seasonally_adjusted, seasonal_adjustment,
-		   units.long_label, units.short_label, data_portal_name, percent, s.real,
+		   units.long_label, units.short_label, s.dataPortalName, s.percent, s.real,
 		   sources.description, COALESCE(series.source_link, sources.link) AS source_link, source_details.description,
 		   NULL, NULL, NULL, NULL, NULL, NULL, decimals,
 		   geographies.fips, geographies.handle, geographies.display_name, geographies.display_name_short
