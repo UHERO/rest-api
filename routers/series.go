ebfbd4bd239d5d7aec422662e24df9699faa5744
dji <David.Iannucci@hawaii.edu>
2020-10-30 17:03:23 -1000

    add /v1.u routes

--- routers/series.go
+++ routers/series.go
@@ -52,5 +52,22 @@ func SetSeriesRoutes(
 	router.HandleFunc("/v1/series/observations", controllers.GetSeriesObservations(seriesRepository, cacheRepository)).Methods("GET").Queries(
 		"id", "{id:[0-9]+}",
 	)
+
+	router.HandleFunc("/v1.u/series", controllers.GetSeriesByName(seriesRepository, cacheRepository)).Methods("GET").Queries(
+		"name", "{name:.+}",
+		"u",	"{universe:[A-Za-z0-9]+}",
+		"expand", "{exp:[a-z]+}",
+	)
+	router.HandleFunc("/v1.u/series", controllers.GetSeriesByName(seriesRepository, cacheRepository)).Methods("GET").Queries(
+		"name", "{name:.+}",
+		"expand", "{exp:[a-z]+}",
+	)
+	router.HandleFunc("/v1.u/series", controllers.GetSeriesByName(seriesRepository, cacheRepository)).Methods("GET").Queries(
+		"name", "{name:.+}",
+		"u",	"{universe:[A-Za-z0-9]+}",
+	)
+	router.HandleFunc("/v1.u/series", controllers.GetSeriesByName(seriesRepository, cacheRepository)).Methods("GET").Queries(
+		"name", "{name:.+}",
+	)
 	return router
 }
